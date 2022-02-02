04da3de56c50f39e8706e1c3f359815db13420d1
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-02-02 17:44:55 +0330

    ok node-sass and bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
+import MyComponents from "./MyComponents";
 
 function App() {
   return (
     <div>
       <h1>HelloWorld</h1>
+      <MyComponents />
     </div>
   );
 }
