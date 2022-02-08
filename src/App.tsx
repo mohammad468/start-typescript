3575b3d09d56b705bd4cc2d1827b946c6baa11ee
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-02-08 17:27:03 +0330

    start again

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.scss";
 
 // ?components
-import InputField from "./components/InputField";
-import TodoList from "./components/TodoList";
-import { Todo } from "./model";
 
 const App: React.FC = () => {
-  const [todo, setTodo] = useState<string>("");
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const handleAdd = (event: React.FormEvent) => {
-    event.preventDefault();
-
-    setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
-    setTodo("");
-  };
-
-  console.log(todos);
 
   return (
     <div>
       <div className="d-flex justify-content-center my-2">
         <h1>HelloWorld</h1>
-      </div>
-      <div className="d-flex justify-content-center my-2">
-        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-      </div>
-      <div className="d-flex justify-content-center my-2">
-        <TodoList setTodos={setTodos} todos={todos} />
       </div>
     </div>
   );
